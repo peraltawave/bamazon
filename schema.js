48e1fc884c907b734e3ca502aaef2871cf3fe672
peraltawave <peraltawave@gmail.com>
2018-12-31 02:14:04 -0700

    new stuff

--- schema.js
+++ schema.js
@@ -14,10 +14,10 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES(10, "Jeans", "Women's Clothing", 20, 15), (11, "Dress", "Women's Clothing", 24, 20),
-(12, "Scarf", "Women's Clothing", 90, 25), (13, "Shoes", "Women's Clothing", 30, 10),
-(14, "Jeans", "Men's Clothing", 20, 15), (15, "Ties", "Men's Clothing", 12, 7),
-(16, "Jacket", "Men's Clothing", 40, 3), (17, "Boots", "Men's Clothing", 30, 9),
-(18, "Shoes", "Kid's Clothing", 20, 15), (19, "Bows", "Kid's Clothing", 2, 18),
-(20, "Balls", "Kid's Toys", 2, 28), (21, "Trucks", "Kid's Toys", 3, 13) ,
-(22, "Doll", "Kid's Toys", 20, 2); 
+VALUES(10, "Mom Jeans", "Mom Clothing", 20, 15), (11, "Dress with Pockets", "Mom Clothing", 24, 20),
+(12, "Scarf", "Mom Clothing", 90, 25), (13, "Shoes", "Mom Clothing", 30, 10),
+(14, "Dad Jeans", "Dad Clothing", 20, 15), (15, "Ties", "Dad Clothing", 12, 7),
+(16, "Jacket", "Dad Clothing", 40, 3), (17, "Boots", "Dad Clothing", 30, 9),
+(18, "Shoes", "Baby Clothing", 20, 15), (19, "Bows", "Baby Clothing", 2, 18),
+(20, "Balls", "Baby Toys", 2, 28), (21, "Trucks", "Baby Toys", 3, 13) ,
+(22, "Doll", "Baby Toys", 20, 2); 
